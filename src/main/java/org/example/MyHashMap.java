38aa14624b4c2b23635f353b5f8977ac79092a57
lightget2099 <aru54krat@gmail.com>
2026-02-12 18:44:28 +0200

    fixed MyHashmap

--- src/main/java/org/example/MyHashMap.java
+++ src/main/java/org/example/MyHashMap.java
@@ -3,42 +3,50 @@ package org.example;
 import java.util.*;
 
 public class MyHashMap<K, V> {
-    private class Node{
+
+    private class Node<K, V> {
         K key;
         V value;
-        Node next;
-        Node(K key, V value){
+        Node<K, V> next;
+
+        Node(K key, V value) {
             this.key = key;
             this.value = value;
         }
     }
+
     private final int CAPACITY = 16;
 
     @SuppressWarnings("unchecked")
     private Node<K, V>[] bucket = (Node<K, V>[]) new Node[CAPACITY];
-    int size;
 
-    public void put(K key, V value){
+    private int size;
+
+
+    public void put(K key, V value) {
         int index = key.hashCode() % CAPACITY;
-        Node current = bucket[index];
-        while(current != null){
-            if(current.key.equals(key)){
+        Node<K, V> current = bucket[index];
+
+        while (current != null) {
+            if (current.key.equals(key)) {
                 current.value = value;
                 return;
             }
             current = current.next;
         }
-        Node node = new Node(key, value);
+
+        Node<K, V> node = new Node<>(key, value);
         node.next = bucket[index];
         bucket[index] = node;
         size++;
     }
 
-    public V get(K key){
+    public V get(K key) {
         int index = key.hashCode() % CAPACITY;
-        Node current = bucket[index];
-        while(current != null){
-            if(current.key.equals(key)){
+        Node<K, V> current = bucket[index];
+
+        while (current != null) {
+            if (current.key.equals(key)) {
                 return current.value;
             }
             current = current.next;
@@ -46,15 +54,19 @@ public class MyHashMap<K, V> {
         return null;
     }
 
+
     public V remove(K key) {
         int index = key.hashCode() % CAPACITY;
-        Node current = bucket[index];
-        Node prev = null;
+        Node<K, V> current = bucket[index];
+        Node<K, V> prev = null;
+
         while (current != null) {
             if (current.key.equals(key)) {
                 if (prev == null) {
                     bucket[index] = current.next;
-                } else prev.next = current.next;
+                } else {
+                    prev.next = current.next;
+                }
                 size--;
                 return current.value;
             }
@@ -63,14 +75,17 @@ public class MyHashMap<K, V> {
         }
         return null;
     }
-    public int size(){
+
+
+    public int size() {
         return size;
     }
 
-    public void clear(){
-        for(int i = 0; i < CAPACITY; i++){
+
+    public void clear() {
+        for (int i = 0; i < CAPACITY; i++) {
             bucket[i] = null;
         }
         size = 0;
     }
-    }
+}
